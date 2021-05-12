5b391ee75a8485f0a67a4266e981b2dddf911d67
Soumi Bardhan <soumibardhan10@gmail.com>
2021-05-12 19:32:08 +0530

    config

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    endpoint: 'https://self-hosted.gitlab/api/v4/',
+    endpoint: 'https://api.github.com/repos/',
     token: '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',
     platform: 'github',
     logLevel: 'debug',
     onboardingConfig: {
       extends: ['config:base'],
     },
-    repositories: ['username/repo', 'orgname/repo'],
+    repositories: ['Soumi7/renovate-docker'],
   };
